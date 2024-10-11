d962ddf6483d1cc036036e64bd1d409b6ad3150d
naren4488 <narendrakajla77@gmail.com>
2024-10-11 16:55:28 +0530

    accordion implemented

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import TodoList from './components/TodoList.vue'
 import RandomQuotes from './components/RandomQuotes.vue'
+import AccordionComponent from './components/AccordionComponent.vue'
 </script>
 
 <template>
 <!-- <TodoList/> -->
- <RandomQuotes/>
+ <!-- <RandomQuotes/> -->
+  <AccordionComponent/>
 
 </template>
 
