4579a0b7d29f44e80160d1a6932d9b99b10414d4
naren4488 <narendrakajla77@gmail.com>
2024-10-12 08:34:31 +0530

    formvalidation implemented

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script setup>
-import TodoList from './components/TodoList.vue'
-import RandomQuotes from './components/RandomQuotes.vue'
-import AccordionComponent from './components/AccordionComponent.vue'
+import TodoList from "./components/TodoList.vue";
+import RandomQuotes from "./components/RandomQuotes.vue";
+import AccordionComponent from "./components/AccordionComponent.vue";
+import FormValidation from "./components/FormValidation.vue";
 </script>
 
 <template>
-<!-- <TodoList/> -->
- <!-- <RandomQuotes/> -->
-  <AccordionComponent/>
-
+  <!-- <TodoList/> -->
+  <!-- <RandomQuotes/> -->
+  <!-- <AccordionComponent/> -->
+  <FormValidation />
 </template>
-
-
