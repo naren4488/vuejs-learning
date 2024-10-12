4400ecba7a625b16930b6729f33ef6320622ef2b
naren4488 <narendrakajla77@gmail.com>
2024-10-12 15:31:12 +0530

    password generator implemented

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@ import TodoList from "./components/TodoList.vue";
 import RandomQuotes from "./components/RandomQuotes.vue";
 import AccordionComponent from "./components/AccordionComponent.vue";
 import FormValidation from "./components/FormValidation.vue";
+import PasswordGenerator from './components/PasswordGenerator.vue'
 </script>
 
 <template>
   <!-- <TodoList/> -->
   <!-- <RandomQuotes/> -->
   <!-- <AccordionComponent/> -->
-  <FormValidation />
+  <!-- <FormValidation /> -->
+   <PasswordGenerator/>
 </template>
